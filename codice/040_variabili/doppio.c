d83d614b11a0121490152c4ae84b3eaa68177927
Marco Alberti <malbertife@gmail.com>
2023-10-02 09:29:05 +0200

    soluzioni

---
+++ codice/040_variabili/doppio.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+main(){
+    // definisce una variabile intera di nome v
+    int v;
+    // assegna a v il valore 4;
+    v = 4;
+    // stampa in output il doppio di v;
+    printf("%d\n", v * 2);
+}
---
+++ codice/040_variabili/ripasso/divisione.c
@@ -0,0 +1,7 @@
+// stampa quoziente e resto di 22 e 7
+#include <stdio.h>
+
+main() {
+  printf("quoziente: %d\n", 22 / 7);
+  printf("resto: %d\n", 22 % 7);
+}
