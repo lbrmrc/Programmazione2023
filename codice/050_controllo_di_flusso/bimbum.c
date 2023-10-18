a85edb57ef56b26c230d6386aa878f3384da2d4f
Marco Alberti <malbertife@gmail.com>
2023-10-18 12:49:09 +0200

    traccia bimbum

---
+++ codice/050_controllo_di_flusso/bimbum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main(){
+    int i;
+
+    for (i=1; i<= 100; i++){
+    // Si = [1,2,...,99,100]
+    
+    printf("bim ");
+    printf("bum ");
+    printf("bimbum ");
+    printf("%d ", i);
+    }
+}
