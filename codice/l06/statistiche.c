9a475b43434199987bdffbe54ac951fb5bd8d9d8
Marco Alberti <malbertife@gmail.com>
2023-11-16 15:34:25 +0100

    min, max, media

--- codice/l06/statistiche.c
+++ codice/l06/statistiche.c
@@ -5,7 +5,9 @@ int main() {
   int a[DIM];
   int numero;
   int i;
-  int dl; // dimensione logica dell'array
+  int min, max;
+  int dl;  // dimensione logica dell'array
+  int somma;
 
   // input in un array di max DIM interi non negativi (negativo termina)
   dl = 0;
@@ -19,10 +21,24 @@ int main() {
   }
 
   // stampa minimo
+  min = 0;
+  for (i = 1; i < dl; i++)
+    if (a[i] < a[min])
+      min = i;
+  printf("Minimo: %d\n", a[min]);
 
   // stampa massimo
+  max = 0;
+  for (i = 1; i < dl; i++)
+    if (a[i] > a[max])
+      max = i;
+  printf("Massimo: %d\n", a[max]);
 
   // stampa media aritmetica
+  somma = 0;
+  for (i = 0; i < dl; i++)
+    somma += a[i];
+  printf("Media aritmetica: %f\n", (float)somma / dl);
 
   // stampa dei numeri più frequenti (mode)
 }
