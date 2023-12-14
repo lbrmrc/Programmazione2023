cf98b30907f4446393d16b1ad1809abd88423f41
Marco Alberti <malbertife@gmail.com>
2023-12-14 10:34:01 +0100

    esercizio

--- codice/230_liste_collegate/calorie/listaAlimento.c
+++ codice/230_liste_collegate/calorie/listaAlimento.c
@@ -4,48 +4,23 @@
 
 #include "listaAlimento.h"
 
-Lista* ricerca(Lista* pl) {
-  while (*pl)
-    pl = &(*pl)->next;
-
-  return pl;
-}
-
-void insTesta(Lista* pl, Dato d) {
-  Nodo* aux = (Nodo*)malloc(sizeof(Nodo));
-  aux->dato = d;
-  aux->next = *pl;
-  *pl = aux;
-}
-
-void insCoda(Lista* pl, Dato d) {
-  pl = ricerca(pl);
-  insTesta(pl, d);
-}
-
 void caricaCalorie(char nomeFileBinario[], Lista* pl) {
   int numero;
-  Dato d;
   FILE* fb;
   fb = fopen(nomeFileBinario, "rb");
   if (fb == NULL) {
     printf("Impossibile aprire %s\n", nomeFileBinario);
     exit(3);
   }
-
-  *pl = NULL;
-  while (fread(&d, sizeof(Dato), 1, fb) == 1)
-    insCoda(pl, d);
-
+  pl->n_elementi = fread(pl->dati, sizeof(Alimento), DIMENSIONE, fb);
   fclose(fb);
 }
 
 float calorieAlimento(char nomeAlimento[], Lista l) {
-  while (l != NULL) {
-    if (strcmp(nomeAlimento, l->dato.nome) == 0)
-      return l->dato.calorie;
-    l = l->next;
-  }
+  int i;
+  for (i = 0; i < l.n_elementi; i++)
+    if (strcmp(nomeAlimento, l.dati[i].nome) == 0)
+      return l.dati[i].calorie;
   printf("Alimento non trovato\n");
   exit(4);
 }
--- codice/230_liste_collegate/calorie/listaAlimento.h
+++ codice/230_liste_collegate/calorie/listaAlimento.h
@@ -3,15 +3,13 @@
 // record
 typedef struct {
   char nome[31];
-  float calorie;  // 100 grammi dell'alimento
-} Dato;
+  float calorie; // 100 grammi dell'alimento
+} Alimento;
 
-typedef struct nodo {
-  Dato dato;
-  struct nodo* next;
-} Nodo;
+typedef struct {
+  int n_elementi;
+  Alimento dati[DIMENSIONE];
+} Lista;
 
-typedef Nodo* Lista;
-
-void caricaCalorie(char nomeFileBinario[], Lista* pl);
+void caricaCalorie(char nomeFileBinario[], Lista *pl);
 float calorieAlimento(char nomeAlimento[], Lista l);
